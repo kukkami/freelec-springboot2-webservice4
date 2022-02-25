eecaa075a10d1e5e9e5cc244d277dd832d525aea
kukkami <kukkami@daum.net>
2022-02-25 16:33:04 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/freelecspringboot2webservice4/FreelecSpringboot2Webservice4Application.java
@@ -0,0 +1,13 @@
+package com.example.freelecspringboot2webservice4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpringboot2Webservice4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelecSpringboot2Webservice4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/freelecspringboot2webservice4/FreelecSpringboot2Webservice4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freelecspringboot2webservice4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringboot2Webservice4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
